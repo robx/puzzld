9274ea6581389c76fc3fd084cd1c6f06aa045753
Robert Vollmert <rob@vllmrt.net>
2020-03-24 18:35:13 +0100

    mvar handling cleanup, mostly
    - should now be exception safe, by using withMVar, modifyMVar
    - give nicer names

--- Main.hs
+++ Main.hs
@@ -10,25 +10,33 @@ import Network.HTTP.Types
 import Network.Wai.Handler.Warp (run)
 import Network.Wai.Handler.WebSockets
 
-type Conns = MVar [Connection]
+type State = [Connection]
 
-app :: Conns -> Application
-app conns req respond = do
+emptyState :: State
+emptyState = []
+
+addConnection :: Connection -> State -> State
+addConnection conn = (conn:)
+
+connections :: State -> [Connection]
+connections = id
+
+app :: MVar State -> Application
+app state req respond = do
     case pathInfo req of
       [] -> respond $ responseLBS
                status200
                [("Content-Type", "text/plain")]
                "Hello, Web!"
-      ("game":_) -> game conns req respond
+      ("game":_) -> game state req respond
 
-game :: Conns -> Application
-game conns = websocketsOr defaultConnectionOptions wsApp backupApp
+game :: MVar State -> Application
+game state = websocketsOr defaultConnectionOptions app backup
   where
-    wsApp :: ServerApp
-    wsApp pending_conn = do
+    app :: ServerApp
+    app pending_conn = do
         conn <- acceptRequest pending_conn
-        cs <- takeMVar conns
-        putMVar conns (conn:cs)
+        modifyMVar_ state (pure . addConnection conn)
         putStrLn $ "accepted connection"
         loop conn
 
@@ -36,18 +44,16 @@ game conns = websocketsOr defaultConnectionOptions wsApp backupApp
         msg <- receiveDataMessage conn
         case msg of
            Text b _ -> do putStrLn $ "received text message: " ++ show b
-                          cs <- takeMVar conns
-                          mapM_ (\ c -> sendTextData c b) cs
-                          putMVar conns cs
-           Binary _ -> putStrLn "received binary message"
+                          withMVar state $ mapM_ (\ c -> sendTextData c b) . connections
+           Binary _ -> putStrLn "ignoring binary message"
         putStrLn $ "looping"
         loop conn
 
-    backupApp :: Application
-    backupApp _ respond = respond $ responseLBS status400 [] "Not a WebSocket request"
+    backup :: Application
+    backup _ respond = respond $ responseLBS status400 [] "not a websocket request"
 
 main :: IO ()
 main = do
     putStrLn $ "http://localhost:8080/"
-    conns <- newMVar ([] :: [Connection])
-    run 8080 (app conns)
+    state <- newMVar emptyState
+    run 8080 (app state)
