4bcf75d2919ef4e209ed026fd99203270e99e6da
Robert Vollmert <rob@vllmrt.net>
2020-03-24 18:36:00 +0100

    fmt

--- Main.hs
+++ Main.hs
@@ -3,12 +3,12 @@
 module Main where
 
 import Control.Concurrent
-import Network.WebSockets
 import Data.Text
-import Network.Wai
 import Network.HTTP.Types
+import Network.Wai
 import Network.Wai.Handler.Warp (run)
 import Network.Wai.Handler.WebSockets
+import Network.WebSockets
 
 type State = [Connection]
 
@@ -16,44 +16,45 @@ emptyState :: State
 emptyState = []
 
 addConnection :: Connection -> State -> State
-addConnection conn = (conn:)
+addConnection conn = (conn :)
 
 connections :: State -> [Connection]
 connections = id
 
 app :: MVar State -> Application
 app state req respond = do
-    case pathInfo req of
-      [] -> respond $ responseLBS
-               status200
-               [("Content-Type", "text/plain")]
-               "Hello, Web!"
-      ("game":_) -> game state req respond
+  case pathInfo req of
+    [] ->
+      respond $
+        responseLBS
+          status200
+          [("Content-Type", "text/plain")]
+          "Hello, Web!"
+    ("game" : _) -> game state req respond
 
 game :: MVar State -> Application
 game state = websocketsOr defaultConnectionOptions app backup
   where
     app :: ServerApp
     app pending_conn = do
-        conn <- acceptRequest pending_conn
-        modifyMVar_ state (pure . addConnection conn)
-        putStrLn $ "accepted connection"
-        loop conn
-
+      conn <- acceptRequest pending_conn
+      modifyMVar_ state (pure . addConnection conn)
+      putStrLn $ "accepted connection"
+      loop conn
     loop conn = do
-        msg <- receiveDataMessage conn
-        case msg of
-           Text b _ -> do putStrLn $ "received text message: " ++ show b
-                          withMVar state $ mapM_ (\ c -> sendTextData c b) . connections
-           Binary _ -> putStrLn "ignoring binary message"
-        putStrLn $ "looping"
-        loop conn
-
+      msg <- receiveDataMessage conn
+      case msg of
+        Text b _ -> do
+          putStrLn $ "received text message: " ++ show b
+          withMVar state $ mapM_ (\c -> sendTextData c b) . connections
+        Binary _ -> putStrLn "ignoring binary message"
+      putStrLn $ "looping"
+      loop conn
     backup :: Application
     backup _ respond = respond $ responseLBS status400 [] "not a websocket request"
 
 main :: IO ()
 main = do
-    putStrLn $ "http://localhost:8080/"
-    state <- newMVar emptyState
-    run 8080 (app state)
+  putStrLn $ "http://localhost:8080/"
+  state <- newMVar emptyState
+  run 8080 (app state)
