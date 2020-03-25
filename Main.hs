b4b075c790ebfc3d43f6b74b60bd8ef9f1925c89
Robert Vollmert <rob@vllmrt.net>
2020-03-25 18:05:49 +0100

    introduce my own log function, with context

--- Main.hs
+++ Main.hs
@@ -22,17 +22,23 @@ emptyRooms = Map.empty
 data App
   = App
       { appLogFunc :: !LogFunc,
+        appLogContext :: [Utf8Builder],
         appRooms :: !(MVar Rooms)
       }
 
 instance HasLogFunc App where
   logFuncL = lens appLogFunc (\x y -> x {appLogFunc = y})
 
+info :: Utf8Builder -> RIO App ()
+info msg = do
+  logContext <- view $ to appLogContext
+  logInfo $ mconcat (map (\c -> c <> ": ") logContext) <> msg
+
 runApp :: RIO App () -> IO ()
 runApp inner = runSimpleApp $ do
   logFunc <- view logFuncL
   rooms <- newMVar emptyRooms
-  let app = App {appLogFunc = logFunc, appRooms = rooms}
+  let app = App {appLogFunc = logFunc, appLogContext = [], appRooms = rooms}
   runRIO app inner
 
 getRoom :: Key -> RIO App (MVar Room)
@@ -42,7 +48,7 @@ getRoom key = do
     case Map.lookup key rooms of
       Just room -> return (rooms, room)
       Nothing -> do
-        logInfo $ "creating new room: " <> display key
+        info $ "creating new room: " <> display key
         room <- newMVar emptyRoom
         return $ (Map.insert key room rooms, room)
 
@@ -68,7 +74,7 @@ broadcastMessage msg room = do
       (liftIO (WebSockets.sendTextData conn msg) >> return (Just conn))
         `catch` ( \e -> case e of
                     WebSockets.ConnectionClosed -> do
-                      logInfo $ "dropping closed connection"
+                      info $ "dropping closed connection"
                       return Nothing
                     _ -> throwIO e
                 )
@@ -93,18 +99,18 @@ game room = websocketsOr WebSockets.defaultConnectionOptions handleConn fallback
     handleConn pendingConn = do
       conn <- liftIO $ WebSockets.acceptRequest pendingConn
       modifyMVar_ room (pure . addConnection conn)
-      logInfo $ "accepted connection"
+      info $ "accepted connection"
       forever (handleMessage conn)
     handleMessage conn = do
       msg <- liftIO $ WebSockets.receiveDataMessage conn
       case msg of
         WebSockets.Text b _ -> do
-          logInfo $ "received text message: " <> displayBytesUtf8 (BL.toStrict b)
+          info $ "received text message: " <> displayBytesUtf8 (BL.toStrict b)
           modifyMVar_ room $ broadcastMessage b
-        WebSockets.Binary _ -> logInfo "ignoring binary message"
+        WebSockets.Binary _ -> info "ignoring binary message"
     fallback _ respond = respond $ Wai.responseLBS status400 [] "not a websocket request"
 
 main :: IO ()
 main = runApp $ do
-  logInfo $ "listening on port 8787"
+  info $ "listening on port 8787"
   run 8787 toplevel
